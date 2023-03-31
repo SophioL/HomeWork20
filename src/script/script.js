a9a88b955dbb67eb61f661b1b3613828cfbfc5e9
Sophio <sofolaz@gmail.com>
2023-03-31 17:23:44 +0400

    update2

---
+++ src/script/script.js
@@ -0,0 +1,14 @@
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyDh7Gdvl7g4LDvkjjOf_ctAOUhlqRZoMTM",
+    authDomain: "authentication-app-8a27f.firebaseapp.com",
+    databaseURL: "https://authentication-app-8a27f-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "authentication-app-8a27f",
+    storageBucket: "authentication-app-8a27f.appspot.com",
+    messagingSenderId: "656694224339",
+    appId: "1:656694224339:web:307ea5ef4968975d577b26"
+  };
+
+  const app = initializeApp(firebaseConfig);
